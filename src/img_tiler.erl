3e154c87d3b0fbc7fea9f97722a54c957f1b2508
refactor <mjollnir.ray@gmail.com>
2012-06-15 19:35:26 +0800

    move scan-img continuation to img_tiler module

--- src/img_tiler.erl
+++ src/img_tiler.erl
@@ -2,11 +2,36 @@
 
 -include("global_grid.hrl").
 
-%% API
+
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+%% export for eunit
 -export([
-         calc_tiles_enclosure/0,
+         calc_tiles_enclosure/0
+        ]).
+-endif.
+
+%% API
+-export([scan_img/0,
          copyout_tile_for/3
         ]).
+
+-record(tile_position, {current_tile_x :: integer(), 
+                        current_tile_y :: integer(), 
+                        tile_zoom      :: byte(),
+                        tile_enclosure :: global_grid:encluse()}).
+
+scan_img() ->
+    {TileZoom, TileEnclosure} = calc_tiles_enclosure(),
+    {StartTileX, StartTileY, _, _} = TileEnclosure,
+    {continue, {StartTileX, StartTileY, TileZoom},
+               fun() ->
+                    scan_img(
+                             #tile_position{current_tile_x = StartTileX, 
+                                            current_tile_y = StartTileY, 
+                                            tile_zoom      = TileZoom,
+                                            tile_enclosure = TileEnclosure})
+               end}.
 
 -spec copyout_tile_for(integer(), integer(), byte()) -> 
     {ok, reference()} | {error, string()}.
@@ -20,6 +45,42 @@ copyout_tile_for(Tx, Ty, Tz) ->
     gdal_nif:copyout_tile(RasterImg, Rb, Wb).
 
 
+%% =============================================================================
+%% private functions
+%% =============================================================================
+
+%% @doc Continuation
+scan_img(#tile_position{current_tile_x = MaxX,
+                        current_tile_y = MaxY,
+                        tile_zoom      = _TileZoom,
+                        tile_enclosure = {_,_, MaxX,MaxY}}) ->
+    done;
+scan_img(#tile_position{current_tile_x = MaxX,
+                        current_tile_y = Y,
+                        tile_zoom      = TileZoom,
+                        tile_enclosure ={MinX,MinY,MaxX,MaxY}}) ->
+    {continue, {MinX, Y + 1, TileZoom},
+                fun() ->
+                    scan_img(
+                             #tile_position{current_tile_x = MinX,
+                                            current_tile_y = Y + 1,
+                                            tile_zoom      = TileZoom,
+                                            tile_enclosure = {MinX,MinY,MaxX,MaxY}})
+                end};
+scan_img(#tile_position{current_tile_x = X,
+                        current_tile_y = Y,
+                        tile_zoom      = TileZoom,
+                        tile_enclosure={MinX,MinY,MaxX,MaxY}}) ->
+    {continue, {X + 1, Y, TileZoom},
+                fun() ->
+                    scan_img(
+                             #tile_position{current_tile_x = X + 1,
+                                            current_tile_y = Y,
+                                            tile_zoom      = TileZoom,
+                                            tile_enclosure = {MinX,MinY, MaxX, MaxY}})
+                end}.
+
+
 %% @doc calculate the tiles enclosure of the img Raster in a specified zoom level
 %% the zoom level is dicided by the img precision which is defined in RasterInfo
 %% and used for base_tiles of the img
@@ -29,11 +90,6 @@ calc_tiles_enclosure() ->
     SpatialEnclosure = global_grid:get_img_coordinates_enclosure(RasterInfo),
     TileEnclosure = global_grid:calc_tminmax(ProfileMod, SpatialEnclosure, Tmaxz),
     {Tmaxz, TileEnclosure}.
-
-
-%% =============================================================================
-%% private functions
-%% =============================================================================
 
 %% @doc Get the minimal and maximal zoom level
 %% minimal zoom level: map covers area equivalent to one tile
